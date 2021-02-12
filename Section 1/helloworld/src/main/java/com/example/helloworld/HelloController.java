3c902f5c858a6f2f702d8bc39ed081085f2f156d
ashish choudhary <aashish.choudhary1@gmail.com>
2021-02-13 00:22:50 +0530

    adding Dockerfile and k8s resources

--- Section 1/helloworld/src/main/java/com/example/helloworld/HelloController.java
+++ Section 1/helloworld/src/main/java/com/example/helloworld/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloWorldController {
+public class HelloController {
   @GetMapping("/hello")
   public String hello(@RequestParam(defaultValue = "John", name = "name", required = false) String name, Model model) {
     model.addAttribute("name", name);
